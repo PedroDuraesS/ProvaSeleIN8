f7c247343a4bbb87ecbd35d191cced2f3d66c54b
PedroDuraesS <102993693+PedroDuraesS@users.noreply.github.com>
2022-07-16 08:26:45 -0300

    list_cadas

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function CInputs()
+{
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let date = document.getElementById("date").value;
+    let tel = document.getElementById("tel").value;
+
+    console.log(nome);
+    console.log(email);
+    console.log(date);
+    console.log(tel);
+
+}
+
 
